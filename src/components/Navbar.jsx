60c6823348ce2d8e682a7daa282a9767e9dbfd76
Stone Comstock <comstockstone@yahoo.com>
2022-12-06 17:37:51 -0800

    Navbar routing complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,22 @@ import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
 import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
             <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Explore</p>
           </li>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
             <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Offers</p>
           </li>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
             <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Profile</p>
           </li>
