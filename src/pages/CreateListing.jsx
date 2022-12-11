af7921811e6a6d2f7f82c1e40f20e2600c4e7efa
Stone Comstock <comstockstone@yahoo.com>
2022-12-10 17:16:23 -0800

    Enabled google geolocation to get lat and long values of house

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { toast } from 'react-toastify';
 
 const CreateListing = () => {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -60,10 +61,54 @@ const CreateListing = () => {
     };
   }, [isMounted]);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData);
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error('Discounted Price must be less than regular price!');
+      return;
+    }
+    // Check to make sure the user didn't upload more than 6 images.
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error('Maximum 6 images allowed.');
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+
+      const data = await response.json();
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0].formatted_address;
+
+      // If location is undefined or invalid, notify user and return
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false);
+        toast.error('Please enter a valid address');
+        return;
+      }
+    } else {
+      // If geolocation is not enabled, use manual form input data for long and lat values
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+    }
+    setLoading(false);
+    console.log(geolocation);
   };
+
   const onMutate = (e) => {
     // Set boolean variable based on target values. Will be used to update state
     let boolean = null;
