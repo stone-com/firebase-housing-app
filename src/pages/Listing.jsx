98a6240c12a077eb9cadd7432a4905aeac44c37c
Stone Comstock <comstockstone@yahoo.com>
2023-02-05 15:58:31 +0700

    Basic structure of listing page done

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -64,6 +64,31 @@ const Listing = () => {
             ${listing.regularPrice - listing.discountedPrice} discount
           </p>
         )}
+        <ul className='listingDetailsList'>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} bedrooms`
+              : '1 Bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} bathrooms`
+              : '1 bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+        <p className='listingLocationTitle'>Location</p>
+        {/* Map goes here */}
+        {/* Show contact button if the listing is not the current user's listing */}
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className='primaryButton'
+          >
+            Contact Landlord
+          </Link>
+        )}
       </div>
     </main>
   );
