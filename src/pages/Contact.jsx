de304bbc09c604e84768ef2fad822b3f1b078b4a
Stone Comstock <comstockstone@yahoo.com>
2023-02-05 19:37:45 +0700

    Contact page done, added email functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,8 +10,59 @@ const Contact = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const params = useParams();
-  useEffect(() => {}, [params.landlordId]);
 
-  return <div>Contact</div>;
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, 'users', params.landlordId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error('Could not get landlord data');
+      }
+    };
+    getLandlord();
+  }, [params.landlordId]);
+
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <div className='pageContainer'>
+      <header>
+        <p className='pageHeader'>Contact Landlord</p>
+      </header>
+      {landlord !== null && (
+        <main>
+          <div className='contactLandlord'>
+            <p className='landlordName'>Contact {landlord?.name}</p>
+          </div>
+          <form className='messageForm'>
+            <div className='messageDiv'>
+              <label htmlFor='message' className='messageLabel'>
+                Message
+              </label>
+              <textarea
+                name='message'
+                id='message'
+                className='textarea'
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                'listingName'
+              )}&body=${message}`}
+            >
+              <button className='primaryButton'>Send Message</button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
+  );
 };
 export default Contact;
